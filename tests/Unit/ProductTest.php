37d4fc9a2880652a716db54be7e5a548c308564b
Ridwan Kasim <kasimridwan@Ridwan-Kasim.local>
2022-12-04 19:08:52 +0100

    added test cases for both 15 & 30 percent discounts

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -16,4 +16,24 @@ class ProductTest extends TestCase
 
         $this->assertEquals(5000, $result->final_price);
     }
+
+    public function test_that_a_discount_of_15_percent_is_applied()
+    {
+        $sku = "000003";
+        $category = "sandals";
+        $price = 5000;
+        $result = (new ProductService())->calculateDiscount($category, $sku, $price);
+
+        $this->assertEquals(750, $result->final_price);
+    }
+
+    public function test_that_a_discount_of_30_percent_is_applied()
+    {
+        $sku = "000001";
+        $category = "boots";
+        $price = 5000;
+        $result = (new ProductService())->calculateDiscount($category, $sku, $price);
+
+        $this->assertEquals(1500, $result->final_price);
+    }
 }
